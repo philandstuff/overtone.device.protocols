b0870d80914f9a3bc2b25394925e76ccecb9214d
Philip Potter <philipgpotter@gmail.com>
2012-04-19 23:37:56 +0200

    fix problem of duplicated function names across multiple protocols; introduce Dimensions protocol

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/device.protocols "0.0.3"
+(defproject overtone/device.protocols "0.0.4-SNAPSHOT"
   :description "Abstraction for external controllers such as grids, sliders, pots and LEDs."
   :url "https://github.com/overtone/overtone.device.protocols"
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/overtone/device/protocols.clj
+++ src/overtone/device/protocols.clj
@@ -1,13 +1,7 @@
 (ns overtone.device.protocols)
 
-(defprotocol GridInput
+#_(defprotocol GridInput
   "A generic representation of a grid of buttons."
-  (width [this]
-    "Returns the number of elements of the grid width-wise. This value
-    is not expected to change.")
-  (height [this]
-    "Returns the number of elements of the grid height-wise. This
-    value is not expected to change.")
   (on-action [this f]
     "Registers a callback fn, to be called whenever one of the buttons
      is pressed or released. If a handler has already been registered
@@ -17,21 +11,47 @@
      is a keyword from the set #{:pressed :released} and x and y are
      the coords of the button being pressed."))
 
-(defprotocol GridDisplay
-  "A generic representation of a grid of lights."
+(defprotocol Dimensions
+  "A rectangular grid of elements."
   (width [this]
     "Returns the number of elements of the grid width-wise. This value
     is not expected to change.")
   (height [this]
     "Returns the number of elements of the grid height-wise. This
-    value is not expected to change.")
-  (light-set [this x y colour]
+    value is not expected to change."))
+
+(defprotocol GridDisplay
+  "A generic representation of a grid of lights. Entities which extend
+   GridDisplay should also extend Dimensions."
+  (light-on [this x y]
+    "Illuminates the light at position [x y].")
+  (light-on-all [this]
+    "Illuminates all lights")
+  (light-off [this x y]
+    "Extinguishes the light at position [x y].")
+  (light-off-all [this]
+    "Extinguishes all lights")
+  (light-frame [this leds]
+    "Update the entire field of lights. The rows arg is a map of coords
+    to truthy or falsey values.
+
+    For example, if you have a 2x2 grid, you could update all of the
+    lights like this:
+
+    (light-frame grid {[0 0] true  [1 0] false
+                       [0 1] false [1 1] true})"))
+
+(defprotocol ColourGridDisplay
+  "A generic representation of a grid of lights which can be
+   illuminated in multiple different colours. Entities which extend
+   ColourGridDisplay should also extend Dimensions."
+  (light-colour [this x y colour]
     "Set the light at position [x y] to the given colour. Color 0 is
     off, > 0 represents a specific palette colour. Subject to
     change.")
-  (light-set-all [this colour]
+  (light-colour-all [this colour]
     "Sets all light to a single colour or off if 0.")
-  (light-frame [this leds]
+  (light-colour-frame [this leds]
     "Update the entire field of lights. The rows arg is a map of coords
     to colour values, the same as in led-set. Any unspecified coords
     should default to 0.
@@ -42,7 +62,7 @@
     (light-frame grid {[0 0] 1 [1 0] 0
                        [0 1] 0 [1 1] 1})"))
 
-(defprotocol Button
+#_(defprotocol Button
   (on-action [this f]
     "Registers a callback fn, to be called when the button is pressed
     or released. If a handler has already been registered, replace the
@@ -51,12 +71,12 @@
    The handler fn should take one arg [action], where action is a
    keyword from the set #{:pressed :released}"))
 
-(defprotocol Light
+#_(defprotocol Light
   (light-set [this colour]
     "Set the light to the given colour. Color 0 is off, > 0 represents a
     specific palette colour. Subject to change."))
 
-(defprotocol RangedInput
+#_(defprotocol RangedInput
   (min-val [this]
     "Returns the minimum value the slider will output. This value is
     not expected to change.")
